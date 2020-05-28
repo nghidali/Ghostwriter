625330db9afe577d12fa96a98b7540dfa8a04985
nghidali <natalie@ghidali.com>
2020-05-28 10:02:33 -0700

    added function to output sentiment scores

--- watson_choose.py
+++ watson_choose.py
@@ -1,12 +1,6 @@
-import os
-import json
-from os.path import join, dirname
+import argparse
 
-import sys
-
-import pathlib
-
-import os
+import numpy as np
 
 from ibm_watson import ToneAnalyzerV3
 
@@ -15,11 +9,9 @@ from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
 
 api_key = "zMJdye9Ex8FZe1HomlcLkGoVSEuqakSCgB1QYIlcTmPO"
 
-example_tones = [[{'score': 0.946588, 'tone_id': 'joy', 'tone_name': 'Joy'}], [{'score': 0.948464, 'tone_id': 'joy', 'tone_name': 'Joy'}, {'score': 0.814556, 'tone_id': 'tentative', 'tone_name': 'Tentative'}], [{'score': 0.984756, 'tone_id': 'tentative', 'tone_name': 'Tentative'}],[{'score': 0.696539, 'tone_id': 'joy', 'tone_name': 'Joy'}], [{'score': 0.807813, 'tone_id': 'joy', 'tone_name': 'Joy'}], [{'score': 0.937679, 'tone_id': 'joy', 'tone_name': 'Joy'}, {'score': 0.624321, 'tone_id': 'analytical', 'tone_name': 'Analytical'}, {'score': 0.655928, 'tone_id': 'tentative', 'tone_name': 'Tentative'}], [{'score': 0.659731, 'tone_id': 'sadness', 'tone_name': 'Sadness'}], [{'score': 0.617241, 'tone_id': 'joy', 'tone_name': 'Joy'}, {'score': 0.973325, 'tone_id': 'tentative', 'tone_name': 'Tentative'}, {'score': 0.832924, 'tone_id': 'analytical', 'tone_name': 'Analytical'}],  [{'score': 0.930272, 'tone_id': 'analytical', 'tone_name': 'Analytical'}, {'score': 0.61457, 'tone_id': 'confident', 'tone_name': 'Confident'}],  [{'score': 0.601071, 'tone_id': 'joy', 'tone_name': 'Joy'}, {'score': 0.722847, 'tone_id': 'tentative', 'tone_name': 'Tentative'}]]
-
-def main():
-    target_emotion = 'joy'
-    in_dial = open('samples.txt', 'r')
+def choose_best_emotion(text_file, emotion):
+    target_emotion = emotion
+    in_dial = open(text_file, 'r')
     dialogue_tones = []
     sentences = []
     for sentence in in_dial:
@@ -27,18 +19,23 @@ def main():
             tone = get_text_sentiment(sentence)
             dialogue_tones.append(tone)
             sentences.append(sentence)
+    print(dialogue_tones)
     best_sentence = -1
     best_score = 0
+    scores = np.zeros(len(sentences))
     for sentence in range(len(dialogue_tones)):
         for sentiment in range(len(dialogue_tones[sentence])):
             el = dialogue_tones[sentence][sentiment]
             if el['tone_id'] == target_emotion:
                 print(sentences[sentence], el['score'])
+                scores[sentence] = el['score']
                 if el['score'] > best_score:
                     best_score = el['score']
                     best_sentence = sentences[sentence]
 
     print('The winner is:', best_sentence)
+    print(scores)
+    return scores
 
 
 def get_text_sentiment(text):
@@ -51,4 +48,14 @@ def get_text_sentiment(text):
     return tone_analyzer.tone(text).result['document_tone']['tones']
 
 if __name__ == '__main__':
-    main()
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--text_file", type=str, default="samples.txt",
+        help="the output of run_pplm"
+    )
+    parser.add_argument(
+        "--emotion", type=str, default="joy",
+        help="One of: anger, fear, joy, sadness, analytical, confident, tentative"
+    )
+    args = parser.parse_args()
+    choose_best_emotion(**vars(args))
