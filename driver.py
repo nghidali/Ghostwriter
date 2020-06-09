0facba804ae9ca151fe6d9ccb30465d20304a4d9
Christine Garver <33207878+christinegarver@users.noreply.github.com>
2020-06-09 11:41:16 -0700

    updated driver

--- driver.py
+++ driver.py
@@ -5,11 +5,9 @@ from semantic_similarity import get_most_similar
 from watson_choose import choose_best_emotion
 
 
-#def driver(input = "my dog died", emotion = "fear", file = "samples.txt", grammar_weight = 1, semantic_weight = 1, sa_weight = 1):
-
-#def driver(output_file, input_file, emotion, grammar_weight, semantic_weight, sa_weight):
 def driver(output_file, input, emotion, grammar_weight, semantic_weight, sa_weight):
 
+    print("\n \n EMOTION : {} \n \n".format(emotion))
     # remove words after end of sentence
     clean_up_samples(output_file)
 
@@ -23,33 +21,32 @@ def driver(output_file, input, emotion, grammar_weight, semantic_weight, sa_weig
 
     # get weights
     grammar_result = get_grammar(file)
-    grammar_weights =  [grammar_weight * i[0] for i in grammar_result]
-    semantic_weights = [i * semantic_weight for i in get_most_similar(input)]
-    sa_weights = [i * sa_weight for i in choose_best_emotion(output_file, emotion)]
+    grammar_weights =  [i[0] for i in grammar_result]#[grammar_weight * i[0] for i in grammar_result]
+    semantic_weights = [i for i in get_most_similar(input)] #[i * semantic_weight for i in get_most_similar(input)]
+    sa_weights = [i for i in choose_best_emotion(output_file, emotion)]#[i * sa_weight for i in choose_best_emotion(output_file, emotion)]
 
 
     norm_sa_weights =  [i/max(sa_weights) for i in sa_weights]
     norm_semantic_weights = [i/max(semantic_weights) for i in semantic_weights]
     norm_grammar_weights = [i/max(grammar_weights) for i in grammar_weights]
 
-    print('\n \n weights : {} .. {} .. {} \n \n'.format(norm_sa_weights, norm_semantic_weights, norm_grammar_weights))
-
+    #print('\n \n weights : sa {} .. \n semantic {} .. \n grammar {} \n \n'.format(norm_sa_weights, norm_semantic_weights, norm_grammar_weights))
 
     total = []
 
     # sum weights
     if len(semantic_weights) != len(grammar_weights) != len(sa_weights):
-        print("diff lengths {} ... {} \n".format(semantic_weights, grammar_weights))
+        print("Error! Different lengths of samples: {} ... {} ... {} \n".format(len(semantic_weights), len(grammar_weights), len(sa_weights)))
 
     for i in range(len(semantic_weights)):
-        total.append(norm_grammar_weights[i] + norm_semantic_weights[i] + norm_sa_weights[i])
+        total.append(grammar_weight*norm_grammar_weights[i] + semantic_weight*norm_semantic_weights[i] + sa_weight*norm_sa_weights[i])
 
     idx = np.argmax(total)
 
     print("\n --------------- Total Scores: {}".format(total))
-    print("\n ---------------- Weights: {} ... {} ... {} \n".format(grammar_weight, semantic_weight, sa_weight))
+    print("\n ---------------- Weights: \n Grammar Weight: {} \n Semantic Weight: {} \n SA Weight: {} \n".format(grammar_weight, semantic_weight, sa_weight))
 
-    print("\n \n ---------------------- index {} ... BEST RESULT {} ----------------------------\n \n".format(idx, lines[idx]))
+    print("\n \n ---------------------- at index {} ... BEST RESULT \n {} ----------------------------\n \n".format(idx, lines[idx]))
 
     return lines[idx]
 
@@ -105,15 +102,15 @@ if __name__ == '__main__':
         help="One of: anger, fear, joy, sadness, analytical, confident, tentative"
     )
     parser.add_argument(
-        "--grammar_weight", type=float, default= 1,
+        "--grammar_weight", type=float, default= 0.8, #1
         help="weight of grammarbot"
     )
     parser.add_argument(
-        "--semantic_weight", type=float, default=0.9,
+        "--semantic_weight", type=float, default=1, #.9
         help="weight of semantic similarity"
     )
     parser.add_argument(
-        "--sa_weight", type=float, default=0.8,
+        "--sa_weight", type=float, default=0.8, #.8
         help="weight of sentiment analysis"
     )
 
